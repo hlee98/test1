5768c6ad6d3ebcde6f9317eefcd832960ec64378
hlee98 <122034870+hlee98@users.noreply.github.com>
2023-02-27 15:46:21 -0600

    Change header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from amplify</h1>
+        <h1>Hello amplify</h1>
         <p><a href="https://hayden-auth.auth.us-east-1.amazoncognito.com/login?client_id=5ah8kmq78mcshfbrnkp52nb7b6&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fmaster.d2hvocywfqepj8.amplifyapp.com%2F">Register or Login</a></p>
         <a
           className="App-link"
